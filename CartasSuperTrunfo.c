2909ed1fac79519fc1f79d57fa9643a63b8e5c7e
jotaPalvess <jpalves1709@gmail.com>
2025-06-15 04:13:39 +0000

    Nível mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,20 +14,22 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     char codigoCarta1[4];
-    int populacao1;
+    unsigned long int populacao1;
     float area1;
     float pib1;
     int pontosTuristicos1;
     float densidadePopulacional1;
     float pibPerCapita1;
+    float superPoder1;
 
     char codigoCarta2[4];
-    int populacao2;
+    unsigned long int populacao2;
     float area2;
     float pib2;
     int pontosTuristicos2;
     float densidadePopulacional2;
     float pibPerCapita2;
+    float superPoder2;
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
@@ -37,7 +39,7 @@ int main() {
     printf ("Digite o código da primeira carta (ex: A01): ");
     scanf ("%s", codigoCarta1);
     printf ("Digite a população: ");
-    scanf ("%d", &populacao1);
+    scanf ("%lu", &populacao1);
     printf ("Digite a área: ");
     scanf ("%f", &area1);
     printf ("Digite o PIB: ");
@@ -48,14 +50,15 @@ int main() {
     // Cálculos da primeira carta
     densidadePopulacional1 = populacao1 / area1;
     pibPerCapita1 = (pib1 * 1000000000)/ populacao1; 
+    superPoder1 = (float) populacao1 + area1 + pib1 + pontosTuristicos1 + pibPerCapita1 + (1 / densidadePopulacional1);
 
 
-    printf ("Cadastro da segunda carta!\n\n");
+    printf ("\n\nCadastro da segunda carta!\n\n");
     
     printf ("Digite o código da segunda carta (ex: B02): ");
     scanf ("%s", codigoCarta2);
     printf ("Digite a população: ");
-    scanf ("%d", &populacao2);
+    scanf ("%lu", &populacao2);
     printf ("Digite a área: ");
     scanf ("%f", &area2);
     printf ("Digite o PIB: ");
@@ -66,27 +69,40 @@ int main() {
     // Cálculos da segunda carta
     densidadePopulacional2 = populacao2 / area2;
     pibPerCapita2 = (pib2 * 1000000000) / populacao2; 
+    superPoder2 = (float) populacao2 + area2 + pib2 + pontosTuristicos2 + pibPerCapita2 + (1 / densidadePopulacional2);
 
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
     printf("\n\nCarta 1!\n\n");
     printf("Código da carta: %s\n", codigoCarta1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %f\n", area1);
-    printf("PIB: %f\n", pib1);
+    printf("População: %lu\n", populacao1);
+    printf("Área: %.2f\n", area1);
+    printf("PIB: %.2f\n", pib1);
     printf("Número de pontos turísticos: %d\n", pontosTuristicos1);
     printf("Densidade Populacional: %.2f\n", densidadePopulacional1);
     printf("PIB per Capita: %.2f\n", pibPerCapita1);
+    printf("Super poder: %.2f\n", superPoder1);
 
     printf("\nCarta 2!\n\n");
     printf("Código da carta: %s\n", codigoCarta2);
-    printf("População: %d\n", populacao2);
-    printf("Área: %f\n", area2);
-    printf("PIB: %f\n", pib2);
+    printf("População: %lu\n", populacao2);
+    printf("Área: %.2f\n", area2);
+    printf("PIB: %.2f\n", pib2);
     printf("Número de pontos turísticos: %d\n", pontosTuristicos2);
     printf("Densidade Populacional: %.2f\n", densidadePopulacional2);
     printf("PIB per Capita: %.2f\n", pibPerCapita2);
+    printf("Super poder: %.2f\n", superPoder2);
+
+    // Comparações
+    printf("\nComparação de atributos!\n\n");
+    printf("Populacao: %d\n", populacao1 > populacao2);
+    printf("Area: %d\n", area1 > area2);
+    printf("PIB: %d\n", pib1 > pib2);
+    printf("Pontos Turísticos: %d\n", pontosTuristicos1 > pontosTuristicos2);
+    printf("Densidade Populacional: %d\n", densidadePopulacional1 < densidadePopulacional2);
+    printf("PIB per Capita: %d\n", pibPerCapita1 > pibPerCapita2);
+    printf("Super Poder: %d\n", superPoder1 > superPoder2);
 
     return 0;
 }
